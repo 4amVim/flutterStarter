be1438e36315a1178375e55cf16fa312ea9efd00
igloo <4429609+4amvim@users.noreply.github.com>
2021-05-20 20:52:14 +0530

    feat: :fire: its done

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,63 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+class Ticker1 extends CustomPainter {
+  static double currentOffset = 0;
+  static double currentStart = 0;
+  // static Ticker1? zeTick;
+
+  Ticker1();
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    print('gonna paint');
+    var xAxis = size.height / 2;
+    canvas
+      ..drawRect(Offset.zero & size, Paint()..color = Colors.white)
+      ..translate(currentStart, 0)
+      ..translate(0, xAxis);
+    // canvas.translate(offset, 0);
+    canvas.translate(currentOffset, 0);
+    // currentOffset = offset;
+
+    ///Add [howMany] number of segments to the tape
+    Canvas _addSegments(Canvas cvs, int howMany) {
+      Rect tensLine(double xOffset) => Offset(xOffset, -25) & Size(2.56, 50);
+      Rect unitLine(double xOffset) => Offset(xOffset, -10) & Size(1.6, 20);
+      var black = Paint()..color = Colors.black;
+      //* Draw segments
+      for (var no = 0; no < howMany; no++) {
+        //* Draw a segment
+        cvs.drawRect(tensLine(no * 100), black);
+        for (var i = 1; i < 10; i++) {
+          cvs.drawRect(unitLine(i * 10 + no * 100), black);
+        }
+        cvs.drawRect(tensLine((no + 1) * 100), black);
+      }
+      return cvs;
+    }
+
+    _addSegments(canvas, 9);
+  }
+
+  @override
+  bool shouldRepaint(Ticker1 oldDelegate) {
+    return false;
+  }
+
+  static offsetBy(double offset) {
+    print('offsetting by $offset');
+    currentOffset = offset;
+    // currentOffset += offset;
+  }
+
+  static void shiftStart() {
+    currentStart = currentOffset + currentStart;
+    currentOffset = 0;
+    print('told to start at $currentStart');
+  }
+}
+
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   String text = 'Go on do it!';
@@ -47,28 +104,36 @@ class _MyHomePageState extends State<MyHomePage> {
                       height: 200, child: Container(color: Colors.blue))),
               Text(text),
               Listener(
+                  //1
                   behavior: HitTestBehavior.opaque,
                   onPointerDown: (details) {
                     initialX = details.localPosition;
-                    print(initialX);
                   },
                   onPointerMove: (details) {
-                    var dx = details.localPosition.dx.toStringAsFixed(2);
-                    var dy = details.localPosition.dy.toStringAsFixed(2);
+                    var poka = details.localPosition.dx - initialX.dx;
                     setState(() {
-                      var poka = details.localPosition - initialX;
-                      offset = poka.dx; //double.parse(dx);
-                      text1 = '$initialX' 'poka.dx:${poka.dx}';
+                      Ticker1.offsetBy(poka);
+                      text1 =
+                          'Start: ${Ticker1.currentStart.toStringAsFixed(1)}'
+                          'offset: ${Ticker1.currentOffset.toStringAsFixed(1)}';
+                      // offset = offset + 1;
                     });
                   },
                   onPointerUp: (_) {
-                    nextTime = Ticker.zeTick!.offset;
+                    // nextTime = T+icker1.zeTick!.offset;
+                    print('current ??' + Ticker1.currentOffset.toString());
+                    setState(() {
+                      Ticker1.shiftStart();
+                      text1 =
+                          'Start: ${Ticker1.currentStart.toStringAsFixed(1)}'
+                          'offset: ${Ticker1.currentOffset.toStringAsFixed(1)}';
+                      // offset = offset + 1;
+                    });
                   },
                   child: SizedBox(
                       height: 150,
-                      child: CustomPaint(
-                          painter: Ticker.offset(offset, nextTime),
-                          child: Container()))),
+                      child:
+                          CustomPaint(painter: Ticker1(), child: Container()))),
               Text(text1)
             ])),
         floatingActionButton: FloatingActionButton(
@@ -77,66 +142,4 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Icon(Icons.add),
         ), // This trailing comma makes auto-formatting nicer for build methods.
       );
-}
-
-/// What we do is, to build a Ticker given a start position,
-class Ticker extends CustomPainter {
-  double offset;
-
-  static Ticker? zeTick;
-  double zeroPosition;
-
-  Ticker(this.offset, this.zeroPosition);
-
-  factory Ticker.offset(offset, start) {
-    if (zeTick == null) {
-      print('pokoka');
-      zeTick = Ticker(offset, 50); //zeTick!.offset + offset);
-    } else {
-      zeTick = Ticker(offset,
-          zeTick!.offset + zeTick!.zeroPosition); //zeTick!.offset + offset);
-    }
-    return zeTick!;
-  }
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    var xAxis = size.height / 2;
-    canvas
-      // ..translate(zeroPosition, 0)
-      ..drawRect(Offset.zero & size, Paint()..color = Color(0xFFFFCA28))
-      ..translate(0, xAxis);
-    canvas.translate(offset, 0);
-    canvas.translate(zeroPosition, 0);
-    print('current offset' + (offset + zeroPosition).toString());
-
-    ///Add [howMany] number of segments to the tape
-    Canvas _addSegments(Canvas cvs, int howMany) {
-      Rect tensLine(double xOffset) => Offset(xOffset, -25) & Size(2.56, 50);
-      Rect unitLine(double xOffset) => Offset(xOffset, -10) & Size(1.6, 20);
-      var black = Paint()..color = Colors.black;
-      //* Draw segments
-      for (var no = 0; no < howMany; no++) {
-        //* Draw a segment
-        cvs.drawRect(tensLine(no * 100), black);
-        for (var i = 1; i < 10; i++) {
-          cvs.drawRect(unitLine(i * 10 + no * 100), black);
-        }
-        cvs.drawRect(tensLine((no + 1) * 100), black);
-      }
-      return cvs;
-    }
-
-    _addSegments(canvas, 9);
-  }
-
-  @override
-  bool shouldRepaint(Ticker oldDelegate) {
-    // if (offset > 10) {
-    //   print('repaint' + (oldDelegate.offset - offset).toString());
-    //   return true;
-    // }
-    // print((oldDelegate.offset - offset).toStringAsFixed(1) + 'Wont ');
-    return false;
-  }
 }
