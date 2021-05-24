a0074307e074b2e58eebab43394d8e9a86247b11
igloo <4429609+4amvim@users.noreply.github.com>
2021-05-24 18:30:08 +0530

    chore: :zap:

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:math';
 import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:vector_math/vector_math_64.dart' show Vector4;
 
 void main() => runApp(MyApp());
 
@@ -10,70 +9,71 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) => MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blue, brightness: Brightness.dark),
-      home: MyHomePage());
+      home: OnboardingPage());
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage();
+class OnboardingPage extends StatefulWidget {
+  OnboardingPage();
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _OnboardingPageState createState() => _OnboardingPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _OnboardingPageState extends State<OnboardingPage> {
   String _debugText = 'Go on do it!';
 
   late Offset touchStart;
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(title: Text('Swipe horizontally')),
-        body: Center(
+      appBar: AppBar(title: Text('Swipe vertically')),
+      body: Center(
           child: Container(
-            width: double.infinity,
-            height: double.infinity,
-            color: Colors.green,
-            child: LayoutBuilder(
-              builder: (BuildContext context, BoxConstraints constraints) =>
-                  Container(
-                color: Colors.blue,
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Tape(width: constraints.maxWidth / 3, height: 200),
-                      Text(_debugText)
-                    ]),
-              ),
-            ),
-          ),
-        ),
-      );
+              width: double.infinity,
+              height: double.infinity,
+              color: Colors.green,
+              child: LayoutBuilder(
+                  builder: (BuildContext context, BoxConstraints constraints) =>
+                      ColoredBox(
+                          color: Colors.blue,
+                          child: Stack(children: [
+                            Align(
+                              alignment: Alignment.centerLeft,
+                              child: Tape(
+                                  width: constraints.maxWidth / 3,
+                                  height: constraints.maxHeight),
+                            ),
+                            Align(
+                                alignment: FractionalOffset(
+                                    constraints.maxWidth / 3,
+                                    constraints.maxHeight / 2),
+                                child: Text(_debugText))
+                          ]))))));
 
   ///
-  Widget Tape({required double width, required double height}) {
-    TapeMeasure.width = width;
-    return Transform.rotate(
-      angle: pi / 2,
-      child: Listener(
-          behavior: HitTestBehavior.opaque,
-          onPointerDown: (details) => touchStart = details.localPosition,
-          onPointerMove: (details) => setState(() {
-                TapeMeasure.offsetBy(
-                    (details.localPosition.dx - touchStart.dx));
-                _debugText = TapeMeasure.string +
-                    ' loopTime:' +
-                    (TapeMeasure.looptime?.inMilliseconds.toString() ??
-                        'undefined');
-              }),
-          onPointerUp: (_) => setState(() {
-                TapeMeasure.shiftStart();
-                _debugText = TapeMeasure.string;
-              }),
-          child: Container(
-              height: width,
-              width: height,
-              child: CustomPaint(painter: TapeMeasure()))),
-    );
-  }
+  Widget Tape({required double width, required double height}) => Listener(
+      behavior: HitTestBehavior.opaque,
+      onPointerDown: (details) => touchStart = details.localPosition,
+      onPointerMove: (details) => setState(() {
+            TapeMeasure.offsetBy((details.localPosition.dy - touchStart.dy));
+            _debugText = TapeMeasure.string +
+                ' loopTime:' +
+                (TapeMeasure.looptime?.inMilliseconds.toString() ??
+                    'undefined');
+          }),
+      onPointerUp: (_) => setState(() {
+            TapeMeasure.shiftStart();
+            _debugText = TapeMeasure.string;
+          }),
+      child: Container(
+          height: height,
+          // width: width,
+          // color: Colors.black,
+          child: CustomPaint(
+            painter: TapeMeasure(),
+            child: Container(
+              width: width,
+            ),
+          )));
 }
 
 class TapeMeasure extends CustomPainter {
@@ -82,37 +82,37 @@ class TapeMeasure extends CustomPainter {
 
   static Duration? looptime;
 
-  static late double width;
-
   @override
   void paint(Canvas canvas, Size size) {
     canvas //? Color the background, centre x-axis and do x-translation
       ..drawRect(Offset.zero & size, Paint()..color = Colors.amber)
-      // ..translate(0, size.height / 2) //? Vertically centre the x-axis
-      ..translate(start, 0) //? Offset the start (to persist between touches)
-      ..translate(offset, 0); //? immidiate offset for current fingering
-    // canvas
-    // ..save()
-    // ..rotate(pi / 2);
+      ..translate(size.width, .0) //? Vertically centre the x-axis
+      ..translate(0, start) //? Offset the start (to persist between touches)
+      ..translate(0, offset) //? immidiate offset for current fingering
+      // canvas
+      // ..save()
+      ..rotate(pi / 2); //* remember that hereon the axes are switched
 
     ///Add [howMany] number of segments to the tape
     Canvas _addSegmentsAbove(Canvas cvs, int howMany, bool showText) {
       //* Some functions to ease modigying drawing bit
       //Draw longer lines for tens
       Rect tensLine(double xOffset) =>
-          Offset(xOffset, 0) & Size(2.56, 0.35 * width);
+          Offset(xOffset, 0) & Size(2.56, 0.35 * size.width);
       //Label them
+      double fontSize = 30; //size.width*0.2>
       void tensText(double xOffset, int no) => TextPainter(
           text: TextSpan(
               text: no.toString(),
-              style: TextStyle(fontSize: width * 0.5, color: Colors.red)),
+              style: TextStyle(fontSize: fontSize, color: Colors.red)),
           textDirection: TextDirection.ltr)
-        ..layout(minWidth: width * 0.2, maxWidth: width * 0.8)
-        ..paint(cvs, Offset(-0.65 * width, xOffset - 0.35 * width));
+        ..layout(minWidth: size.width * 0.2, maxWidth: size.width * 0.8)
+        ..paint(cvs, Offset(-0.65 * size.width, xOffset - 0.35 * size.width));
+      print(size.width * 0.2);
 
       // Draw shorter lines for units
       Rect unitLine(double xOffset) =>
-          Offset(xOffset, 0) & Size(1.6, 0.15 * width);
+          Offset(xOffset, 0) & Size(1.6, 0.15 * size.width);
       var black = Paint()..color = Colors.black;
 
       //* Draw segments
@@ -134,17 +134,17 @@ class TapeMeasure extends CustomPainter {
       return cvs;
     }
 
-    _addSegmentsAbove(canvas, 9, true);
+    _addSegmentsAbove(canvas, 90, true);
 
     ///*Remember that co-ords are [x,y,z,1] to be multiplied from the left
     var startTime = DateTime.now();
 
-    Matrix4 poka = Matrix4.translationValues(0, size.height, 0)
+    Matrix4 poka = Matrix4.translationValues(0, size.width, 0)
       ..scale(1.0, -1.0, 1.0);
     canvas.transform(poka.storage);
 
     looptime = DateTime.now().difference(startTime);
-    _addSegmentsAbove(canvas, 9, false);
+    _addSegmentsAbove(canvas, 90, false);
   }
 
   @override
