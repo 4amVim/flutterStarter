fe6e814abf5028f341cf4ce126fa0dfd0d0d2ac6
igloo <4429609+4amvim@users.noreply.github.com>
2021-05-24 21:49:28 +0530

    feat: :tada: added text reading

--- lib/main.dart
+++ lib/main.dart
@@ -42,11 +42,31 @@ class _OnboardingPageState extends State<OnboardingPage> {
                                   width: constraints.maxWidth / 3,
                                   height: constraints.maxHeight),
                             ),
-                            Align(
-                                alignment: FractionalOffset(
-                                    constraints.maxWidth / 3,
-                                    constraints.maxHeight / 2),
-                                child: Text(_debugText))
+                            Positioned(
+                                left: constraints.maxWidth / 3,
+                                top: constraints.maxHeight / 2,
+                                right: 0,
+
+                                // alignment: AlignmentDirectional(
+                                //     constraints.maxWidth / 3, 50),
+                                child: Text(_debugText,
+                                    softWrap: true,
+                                    style: TextStyle(fontSize: 45))),
+                            Positioned(
+                                left: constraints.maxWidth / 3,
+                                top: constraints.maxHeight / 4,
+                                right: 0,
+
+                                // alignment: AlignmentDirectional(
+                                //     constraints.maxWidth / 3, 50),
+                                child: Text(
+                                    ((-TapeMeasurePaint.start -
+                                                TapeMeasurePaint.offset +
+                                                230) /
+                                            100)
+                                        .toStringAsFixed(2),
+                                    softWrap: true,
+                                    style: TextStyle(fontSize: 45))),
                           ]))))));
 
   ///
@@ -54,29 +74,28 @@ class _OnboardingPageState extends State<OnboardingPage> {
       behavior: HitTestBehavior.opaque,
       onPointerDown: (details) => touchStart = details.localPosition,
       onPointerMove: (details) => setState(() {
-            TapeMeasure.offsetBy((details.localPosition.dy - touchStart.dy));
-            _debugText = TapeMeasure.string +
+            TapeMeasurePaint.offsetBy(
+                (details.localPosition.dy - touchStart.dy));
+            _debugText = TapeMeasurePaint.string +
                 ' loopTime:' +
-                (TapeMeasure.looptime?.inMilliseconds.toString() ??
+                (TapeMeasurePaint.looptime?.inMilliseconds.toString() ??
                     'undefined');
           }),
       onPointerUp: (_) => setState(() {
-            TapeMeasure.shiftStart();
-            _debugText = TapeMeasure.string;
+            TapeMeasurePaint.shiftStart();
+            _debugText = TapeMeasurePaint.string;
           }),
       child: Container(
           height: height,
           // width: width,
           // color: Colors.black,
           child: CustomPaint(
-            painter: TapeMeasure(),
-            child: Container(
-              width: width,
-            ),
+            painter: TapeMeasurePaint(),
+            child: Container(width: width),
           )));
 }
 
-class TapeMeasure extends CustomPainter {
+class TapeMeasurePaint extends CustomPainter {
   static double offset = 0;
   static double start = 0;
 
@@ -84,8 +103,18 @@ class TapeMeasure extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
+    var arrowPath = Path()
+      ..addRect(Offset(size.width / 2, size.height / 3) &
+          Size(size.width, size.height / 80))
+      ..lineTo(size.width / 2, -20 + size.height / 3) //?Line up
+      ..lineTo(-30 + size.width / 2, (163 * size.height / 480))
+      ..lineTo(size.width / 2, 20 + (83 * size.height / 240));
+
     canvas //? Color the background, centre x-axis and do x-translation
       ..drawRect(Offset.zero & size, Paint()..color = Colors.amber)
+      ..translate(-30, 0)
+      ..drawPath(arrowPath, Paint()..color = Colors.white)
+      ..translate(30, 0)
       ..translate(size.width, .0) //? Vertically centre the x-axis
       ..translate(0, start) //? Offset the start (to persist between touches)
       ..translate(0, offset) //? immidiate offset for current fingering
@@ -100,14 +129,14 @@ class TapeMeasure extends CustomPainter {
       Rect tensLine(double xOffset) =>
           Offset(xOffset, 0) & Size(2.56, 0.35 * size.width);
       //Label them
-      double fontSize = 30; //size.width*0.2>
+      double fontSize = 40; //size.width*0.2>
       void tensText(double xOffset, int no) => TextPainter(
           text: TextSpan(
               text: no.toString(),
               style: TextStyle(fontSize: fontSize, color: Colors.red)),
           textDirection: TextDirection.ltr)
         ..layout(minWidth: size.width * 0.2, maxWidth: size.width * 0.8)
-        ..paint(cvs, Offset(-0.65 * size.width, xOffset - 0.35 * size.width));
+        ..paint(cvs, Offset(-0.65 * size.width, xOffset - 0.04 * size.width));
       print(size.width * 0.2);
 
       // Draw shorter lines for units
@@ -134,7 +163,7 @@ class TapeMeasure extends CustomPainter {
       return cvs;
     }
 
-    _addSegmentsAbove(canvas, 90, true);
+    _addSegmentsAbove(canvas, 250, true);
 
     ///*Remember that co-ords are [x,y,z,1] to be multiplied from the left
     var startTime = DateTime.now();
@@ -144,11 +173,11 @@ class TapeMeasure extends CustomPainter {
     canvas.transform(poka.storage);
 
     looptime = DateTime.now().difference(startTime);
-    _addSegmentsAbove(canvas, 90, false);
+    _addSegmentsAbove(canvas, 250, false);
   }
 
   @override
-  bool shouldRepaint(TapeMeasure oldDelegate) => false;
+  bool shouldRepaint(TapeMeasurePaint oldDelegate) => false;
 
   static void offsetBy(double displacement) => offset = displacement;
 
