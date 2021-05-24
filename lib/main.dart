5109f3e34951eb9f86b6f015d920979881decaac
igloo <4429609+4amvim@users.noreply.github.com>
2021-05-24 05:32:29 +0530

    build: :bookmark: test on iOS

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,8 @@ class TapeMeasure extends CustomPainter {
   static double offset = 0;
   static double start = 0;
 
+  static late double width;
+
   @override
   void paint(Canvas canvas, Size size) {
     canvas //? Color the background, centre x-axis and do x-translation
@@ -30,12 +32,16 @@ class TapeMeasure extends CustomPainter {
       // ..translate(0, size.height / 2) //? Vertically centre the x-axis
       ..translate(start, 0) //? Offset the start (to persist between touches)
       ..translate(offset, 0); //? immidiate offset for current fingering
+    // canvas
+    // ..save()
+    // ..rotate(pi / 2);
 
     ///Add [howMany] number of segments to the tape
     Canvas _addSegmentsAbove(Canvas cvs, int howMany, bool showText) {
       //* Some functions to ease modigying drawing bit
       //Draw longer lines for tens
-      Rect tensLine(double xOffset) => Offset(xOffset, 0) & Size(2.56, 50);
+      Rect tensLine(double xOffset) =>
+          Offset(xOffset, 0) & Size(2.56, 0.35 * width);
       //Label them
       void tensText(double xOffset, int no) => TextPainter(
           text: TextSpan(
@@ -43,7 +49,7 @@ class TapeMeasure extends CustomPainter {
               style: TextStyle(fontSize: 40, color: Colors.red)),
           textDirection: TextDirection.ltr)
         ..layout()
-        ..paint(cvs, Offset(xOffset, 50));
+        ..paint(cvs, Offset(-0.6 * width, xOffset - 125));
 
       // Draw shorter lines for units
       Rect unitLine(double xOffset) => Offset(xOffset, 0) & Size(1.6, 20);
@@ -54,7 +60,11 @@ class TapeMeasure extends CustomPainter {
         //* Draw a segment
         cvs.drawRect(tensLine(no * 100), black);
         if (showText) {
-          tensText(no * 100.0, no);
+          // cvs.save();
+          cvs.rotate(-pi / 2);
+          tensText(no * 100, no);
+          cvs.rotate(pi / 2);
+          // cvs.restore();
         }
         for (var i = 1; i < 10; i++) {
           cvs.drawRect(unitLine(i * 10 + no * 100), black);
@@ -68,18 +78,22 @@ class TapeMeasure extends CustomPainter {
     // canvas
     //   ..translate(0, size.height)
     //   ..scale(1, -1);
-    Matrix4 poka = Matrix4.translationValues(0, size.height, 0)
-      ..scale(1.0, -1.0, 1.0);
-    // Matrix4 poka = Matrix4.columns(
-    //         Vector4(1.0, 0.0, 0.0, 0.0),
-    //         Vector4(.0, 1.0, 0.0, 0.5 * size.height),
-    //         Vector4(.0, 0.0, 1.0, 0.0),
-    //         Vector4(.0010, .0, 0.0, 1.0))
-    //     .transposed();
+    // Matrix4 poka = Matrix4.translationValues(0, size.height, 0)
+    //   ..scale(1.0, -1.0, 1.0);
+    var startTime = DateTime.now();
+    for (var i = 0; i < 1e4; i++) {
+      Matrix4 poka = Matrix4.columns(
+              Vector4(1.0, 0.0, 0.0, 0.0),
+              Vector4(0.0, -1.0, 0.0, width),
+              Vector4(0.0, 0.0, 1.0, 0.0),
+              Vector4(i.toDouble() /*8e-4*/, 0.0, 0.0, 1.0))
+          .transposed();
 
-    print(poka.toString() + '\n');
-    canvas.transform(poka.storage);
+      // print(poka.toString() + '\n');
+      canvas.transform(poka.storage);
+    }
     _addSegmentsAbove(canvas, 9, false);
+    // canvas.restore();
   }
 
   @override
@@ -104,29 +118,41 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(title: Text('Swipe horizontally')),
-        body: Center(
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Transform.rotate(
-            angle: 0, //pi / 2,
-            child: Listener(
-                // behavior: HitTestBehavior.opaque,
-                onPointerDown: (details) => touchStart = details.localPosition,
-                onPointerMove: (details) => setState(() {
-                      TapeMeasure.offsetBy(
-                          details.localPosition.dx - touchStart.dx);
-                      _debugText = TapeMeasure.string;
-                    }),
-                onPointerUp: (_) => setState(() {
-                      TapeMeasure.shiftStart();
-                      _debugText = TapeMeasure.string;
-                    }),
-                child: SizedBox(
-                    height: 150,
-                    child: CustomPaint(
-                        painter: TapeMeasure(), child: Container()))),
-          ),
-          Text(_debugText)
-        ])),
+        body: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) => Center(
+              child: Container(
+            color: Colors.blue,
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Tape(width: constraints.maxWidth / 2.5, height: 200),
+                  Text(_debugText)
+                ]),
+          )),
+        ),
       );
+
+  ///
+  Widget Tape({required double width, required double height}) {
+    print('width->>> $width');
+    TapeMeasure.width = width;
+    return Transform.rotate(
+      angle: pi / 2,
+      child: Listener(
+          behavior: HitTestBehavior.opaque,
+          onPointerDown: (details) => touchStart = details.localPosition,
+          onPointerMove: (details) => setState(() {
+                TapeMeasure.offsetBy(details.localPosition.dx - touchStart.dx);
+                _debugText = TapeMeasure.string;
+              }),
+          onPointerUp: (_) => setState(() {
+                TapeMeasure.shiftStart();
+                _debugText = TapeMeasure.string;
+              }),
+          child: Container(
+              height: width,
+              width: height,
+              child: CustomPaint(painter: TapeMeasure()))),
+    );
+  }
 }
